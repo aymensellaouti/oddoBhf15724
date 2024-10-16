befd88e8e872f7fc671780fafa52b16852fb504f
aymensellaouti <aymen.sellaouti@gmail.com>
2024-10-16 10:00:58 +0100

    init canLeaveGuard

---
+++ src/app/todo/guards/can-leave.guard.ts
@@ -0,0 +1,6 @@
+import { CanDeactivateFn } from '@angular/router';
+import { TodoComponent } from '../todo/todo.component';
+
+export const canLeaveGuard: CanDeactivateFn<TodoComponent> = (component, currentRoute, currentState, nextState) => {
+  return true;
+};
