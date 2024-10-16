291a74d440c53621a4e7f62f5a03171992b85ac4
aymensellaouti <aymen.sellaouti@gmail.com>
2024-10-16 11:56:23 +0100

    exemples observable

--- src/app/rxjs/test-observable/test-observable.component.ts
+++ src/app/rxjs/test-observable/test-observable.component.ts
@@ -1,24 +1,44 @@
-import { Component, OnDestroy } from "@angular/core";
+import { Component, inject, OnDestroy } from "@angular/core";
 import { Observable, Subscription, filter, map } from "rxjs";
 import { ToastrService } from "ngx-toastr";
-
+// export type nombreOuChaine = number | string;
+// export type Status = 'success' | 'completed' | 'failed';
 @Component({
-  selector: "app-test-observable",
-  templateUrl: "./test-observable.component.html",
-  styleUrls: ["./test-observable.component.css"],
+  selector: 'app-test-observable',
+  templateUrl: './test-observable.component.html',
+  styleUrls: ['./test-observable.component.css'],
 })
 export class TestObservableComponent {
   firstObservable$: Observable<number>;
-
+  toastr = inject(ToastrService);
   constructor(private toaster: ToastrService) {
     this.firstObservable$ = new Observable((observer) => {
       let i = 5;
-      setInterval(() => {
+      const index = setInterval(() => {
         if (!i) {
           observer.complete();
+          clearInterval(index);
         }
         observer.next(i--);
       }, 1000);
     });
+
+    this.firstObservable$.subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+    });
+    // setTimeout(() => {
+      this.firstObservable$
+      .pipe(
+        map( valeur => valeur * 3)
+      )
+      .subscribe({
+        next: (data) => {
+         this.toastr.info('' + data);
+        },
+        complete: () => this.toastr.error('BOOOM !!!!!!')
+      });
+    // }, 3000)
   }
 }
