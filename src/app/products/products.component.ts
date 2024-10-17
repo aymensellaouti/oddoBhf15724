70c2c07c1e086412c91039db9c6664dbc4d5367c
aymensellaouti <aymen.sellaouti@gmail.com>
2024-10-17 11:54:35 +0100

    starting products Tips

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -17,8 +17,21 @@ import { Settings } from "./dto/product-settings.dto";
   styleUrls: ["./products.component.css"],
 })
 export class ProductsComponent {
+  setting: Settings = {
+    limit: 12,
+    skip: 0,
+  }
+  #settings$ = new BehaviorSubject(this.setting);
   /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
   /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
-  products$!: Observable<Product[]>;
+  products$!: Observable<Product[]> = this.#settings$.pipe(
+
+    // {0, 12}, {12,12}, {24;12}, ....
+  );
   constructor() {}
+
+  moreProducts() {
+    this.setting = { ...this.setting, skip: this.setting.skip + this.setting.limit };
+    this.#settings$.next(this.setting);
+  }
 }
